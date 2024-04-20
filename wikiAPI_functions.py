99182ec02aa66b209c10ce4987fecc43f00309e6
rambomario15 <ramseymakan@gmail.com>
2024-04-20 19:24:08 -0400

    Updated BFS and other methods

--- wikiAPI_functions.py
+++ wikiAPI_functions.py
@@ -12,6 +12,7 @@ class wikiApi:
                       "was", "were", "be", "being", "been", "have", "has", "had", "do", "does", "did", "will", "would",
                       "shall", "should", "can", "could", "may", "might", "must", "ought", "also", "s", "its", "such",
                       "than", "many", "much"}
+        self.stop_words = [word.upper() for word in self.stop_words]
 
     def get_wikipedia_page_links(self, page_title):
         # Fetch the page for the given title
@@ -53,7 +54,7 @@ class wikiApi:
             return "Page not found"
 
         # Clean and split the text into words, lowercasing to standardize
-        words = re.findall(r'\w+', text.lower())
+        words = re.findall(r'\w+', text.upper())
 
         # Create a Counter to count occurrences of each word, excluding stop words
         word_counts = Counter(word for word in words if word not in self.stop_words)
@@ -64,17 +65,29 @@ class wikiApi:
     def split(self, title):
         return [word for word in title.split() if word not in self.stop_words]
 
+    def get_categories(self, page):
+        # Fetch the page for the given title
+        pageEx = self.wiki.page(page)
+        categories = pageEx.categories
+        '''
+        for title in sorted(categories.keys()):
+            print(title)
+        print(len(categories))
+        '''
+        return set(categories.keys())
+
     def get_n_first_similarity_index_of_links(self, current_page, target_page, n):
         # List to store titles that contain any word found in the target page's word frequency list
         links_and_indices = {}
 
         # Creates word frequency dictionary of the target page
         word_frequency = self.get_word_frequency(target_page)
+        #if target_page == "Ukraine": print(word_frequency)
         # Creates list of linked pages in current page
         pageLinks = self.get_wikipedia_page_links(current_page)
 
         # Retrieves linked titles from the current page and splits each title into words
-        title_words = {title: self.split(title) for title in pageLinks}
+        title_words = {title: self.split(title.upper()) for title in pageLinks}
 
         # If target page is in links of current page then the page is found!!!
         #if target_page in pageLinks:
@@ -93,64 +106,12 @@ class wikiApi:
         links_and_indices = dict(sorted(links_and_indices.items(), key=lambda item: item[1], reverse=True))
         return {x: links_and_indices[x] for x in list(links_and_indices)[:n]}
 
-    # New functions
-    def get_categories(self, page):
-        # Fetch the page for the given title
-        pageEx = self.wiki.page(page)
-        categories = pageEx.categories
-        '''
-        for title in sorted(categories.keys()):
-            print(title)
-        print(len(categories))
-        '''
-        return set(categories.keys())
-
-    '''
-    def get_summary(self, page):
-        pageObj = self.wiki.page(page)
-        return pageObj.summary
-    '''
-
-    def bfs_search(self, starting_page, target_page, n):
-        queue = deque([starting_page])  # Queue to manage the frontier pages
-        visited = set()  # Set to keep track of visited pages to avoid cycles
-
-        while queue:
-            current_page = queue.popleft()
-
-            # Check if the current page is the target page
-            if current_page == target_page:
-                return f"Target page '{target_page}' found starting from '{starting_page}'."
-
-            # Skip revisiting pages
-            if current_page in visited:
-                continue
-            visited.add(current_page)
-
-            # Get the top `n` similar linked pages from the current page
-            try:
-                related_links = self.get_n_first_similarity_index_of_links(current_page, target_page, n)
-                print(str(current_page) + " links to " + str(related_links))
-
-            except Exception as e:
-                print(f"Failed to retrieve or process links for {current_page}: {e}")
-                continue
-
-            # Enqueue unvisited linked pages
-            for page, similarity_index in related_links.items():
-                if page not in visited:
-                    queue.append(page)
-
-        return "Target page not found within the connected pages."
-
-
-    '''
     def get_proportion_of_common_categories(self, sourcePage, targetPage):
         sourceCategories = self.get_categories(sourcePage)
         targetCategories = self.get_categories(targetPage)
         if len(targetCategories) == 0: return 0
         return len(sourceCategories & targetCategories) / len(targetCategories)
-    
+
     def get_links_sorted_by_common_categories_with_target(self, page, target):
         links = self.get_wikipedia_page_links(page)
         targetCategories = self.get_categories(target)
@@ -165,12 +126,61 @@ class wikiApi:
             linksAndProportions[link] = proportion
             # print(str(link) + " : " + str(len(sourceCategories)))
         return dict(sorted(linksAndProportions.items(), key=lambda item: item[1], reverse=True))
-    '''
+
+    def get_n_most_connected_links_by_categories(self, current_page, target_page, n):
+        # Look for n * 3 most connected articles by word similarities
+        related_links = self.get_n_first_similarity_index_of_links(current_page, target_page, n*2)
+        targetCategories = self.get_categories(target_page)
+        linksAndProportions = {}
+        for link in links:
+            sourceCategories = self.get_categories(link)
+            proportion = 0
+            if (len(targetCategories) > 0):
+                for category in targetCategories:
+                    if category in sourceCategories: proportion += 1
+                proportion /= len(targetCategories)
+            linksAndProportions[link] = proportion
+            # print(str(link) + " : " + str(len(sourceCategories)))
+        return dict(sorted(linksAndProportions.items(), key=lambda item: item[1], reverse=True))
+
+    def bfs_search(self, starting_page, target_page, n):
+        queue = deque([starting_page])  # Queue to manage the frontier pages
+        visited = set()  # Set to keep track of visited pages to avoid cycles
+
+        while queue:
+            current_page = queue.popleft()
+
+            # Skip revisiting pages
+            if current_page in visited:
+                continue
+            visited.add(current_page)
+
+            # Get the top 'n' similar linked pages from the current page
+            try:
+                related_links = self.get_n_first_similarity_index_of_links(current_page, target_page, n)
+                # Check if the current page is the target page
+                print(str(current_page) + " links to " + str(related_links))
+                if target_page.upper() in [word.upper() for word in related_links.keys()]:
+                    return f"Target page '{target_page}' found starting from '{starting_page}'."
+
+
+            except Exception as e:
+                print(f"Failed to retrieve or process links for {current_page}: {e}")
+                continue
+
+            # Enqueue unvisited linked pages
+            for page, similarity_index in related_links.items():
+                if page not in visited:
+                    queue.append(page)
+
+        return "Target page not found within the connected pages."
+
 
 
 wikiInstance = wikiApi()
 #print(wikiInstance.get_proportion_of_common_categories("University of Georgia", "Bulldog"))
-print(wikiInstance.bfs_search("Mars", "Moon", 4))
+#print(wikiInstance.bfs_search("Mars", "Moon", 4))
+print(wikiInstance.bfs_search("Starbucks", "Strawberry", 10))
 '''
 # Example usage
 
